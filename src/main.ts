9f26fae344b6df262049c1c39ab6dcd63f2426c1
Coraline <cesedji@myges.fr>
2023-06-01 22:05:54 +0200

    Fix : launch api in local environment

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.TCP,
     options: {
-      host: '0.0.0.0',
+      host: process.env.HOST || '0.0.0.0',
       port: Number(process.env.PORT) || 9000,
     },
     logger: ['error', 'warn', 'debug', 'log'] as LogLevel[],
@@ -27,6 +27,7 @@ async function bootstrap() {
   app.useGlobalFilters(new SentryHttpFilter());
 
   await app.listen();
+  console.log(process.env.PORT, '*.'.repeat(10));
 }
 
 bootstrap();
